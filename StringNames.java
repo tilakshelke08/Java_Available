7a2147ef60b510dbacf6b10833ee57beb8e40978
Tilak_Shelke <147398031+tilakshelke08@users.noreply.github.com>
2023-11-20 22:54:10 +0530

    Add String Array Names

---
+++ StringNames.java
@@ -0,0 +1,12 @@
+package SumArray1;
+
+public class StringNames {
+public static void main(String [] Args) {
+	String [] Names= {"Tilak","Pooja","Nandini","Nishant","Amol"};
+	
+	for(String nm:Names) {
+		System.out.println(nm );
+	}
+	
+}
+}
